ea213358f95f14f4df1794ce14f3280ed7e54f29
Zoltan Varga <zolvarga@microsoft.com>
2018-01-23 16:06:37 -0800

    Save temporary structure

--- provisioning_service_client/setup.py
+++ provisioning_service_client/setup.py
@@ -3,14 +3,15 @@ import sys
 
 setup(
     name='azure_iothub_provisioningserviceclient',
-    version='1.0.0',
+    version='0.0.1',
     description='IoT Hub Provisioning Service Client Library',
     license='Apache Software License',
     url='https://github.com/Azure/azure-iot-sdk-python/tree/master/provisioningserviceclient',
     author='aziotclb',
     author_email='aziotclb@microsoft.com',
     long_description='IoT Hub Provisioning Service Client Library for Python',
-    packages=['', 'serviceswagger', 'serviceswagger.models', 'serviceswagger.operations'],
+    install_requires=['msrest'],
+    packages=['provisioningserviceclient', 'provisioningserviceclient.serviceswagger', 'provisioningserviceclient.serviceswagger.models', 'provisioningserviceclient.serviceswagger.operations'],
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
