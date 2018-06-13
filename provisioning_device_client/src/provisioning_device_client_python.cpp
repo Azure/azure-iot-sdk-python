5963d5705337512573ded5955daec705d64c4169
John Spaith <jspaith@windows.microsoft.com>
2018-06-13 12:21:06 -0700

    Bumping versions

--- provisioning_device_client/src/provisioning_device_client_python.cpp
+++ provisioning_device_client/src/provisioning_device_client_python.cpp
@@ -41,7 +41,7 @@
 #define IMPORT_NAME provisioning_device_client
 #endif
 
-#define VERSION_STRING "1.4.0.0b1"
+#define VERSION_STRING "1.4.0.0b2"
 
 #if PY_MAJOR_VERSION >= 3
 #define IS_PY3
--- service/src/iothub_service_client_python.cpp
+++ service/src/iothub_service_client_python.cpp
@@ -34,7 +34,7 @@
 #define IMPORT_NAME iothub_service_client
 #endif
 
-#define VERSION_STRING "1.4.0.0b1"
+#define VERSION_STRING "1.4.0.0b2"
 
 #if PY_MAJOR_VERSION >= 3
 #define IS_PY3
