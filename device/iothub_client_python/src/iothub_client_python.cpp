ede9469c2118f5bfb85127ffc3b90aba78cf3c27
Pierre Cauchois <pierreca@microsoft.com>
2018-09-13 08:45:08 -0700

    Update to latest C SDK - bump to version 1.4.3

--- device/iothub_client_python/src/iothub_client_python.cpp
+++ device/iothub_client_python/src/iothub_client_python.cpp
@@ -52,7 +52,7 @@
 #define IMPORT_NAME iothub_client
 #endif
 
-#define VERSION_STRING "1.4.2"
+#define VERSION_STRING "1.4.3"
 
 #if PY_MAJOR_VERSION >= 3
 #define IS_PY3
--- provisioning_device_client/src/provisioning_device_client_python.cpp
+++ provisioning_device_client/src/provisioning_device_client_python.cpp
@@ -41,7 +41,7 @@
 #define IMPORT_NAME provisioning_device_client
 #endif
 
-#define VERSION_STRING "1.4.2"
+#define VERSION_STRING "1.4.3"
 
 #if PY_MAJOR_VERSION >= 3
 #define IS_PY3
