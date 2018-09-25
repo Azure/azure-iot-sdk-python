94be818e12805b2be430aa7aba66f3695bdd6f3d
Mahesh Rohera <marohera@microsoft.com>
2018-09-25 13:00:31 -0700

    Add copyright license

--- device/samples/edge_downstream_client.py
+++ device/samples/edge_downstream_client.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python
+
+# Copyright (c) Microsoft. All rights reserved.
+# Licensed under the MIT license. See LICENSE file in the project root for
+# full license information.
+
 import sys
 import time
 
