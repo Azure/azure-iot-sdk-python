6b3820a200be4ebab10246aece9f0c92db547550
Bert Kleewein <bertk@microsoft.com>
2018-10-26 10:22:46 -0700

    black formatting

--- azure-iot-hub-devicesdk/azure/iot/hub/devicesdk/iotedge_hsm.py
+++ azure-iot-hub-devicesdk/azure/iot/hub/devicesdk/iotedge_hsm.py
@@ -4,8 +4,8 @@ import os
 import base64
 import json
 
-class IotEdgeHsm(object):
 
+class IotEdgeHsm(object):
     def __init__(self):
         """
         Constructor for instantiating a iot hsm object.  This is an object that can 
@@ -13,19 +13,21 @@ class IotEdgeHsm(object):
         """
         # All of these environment variables are required.  If any are missing,
         # we want this to fail.
-        self.module_id = os.environ['IOTEDGE_MODULEID']
-        self.api_version = os.environ['IOTEDGE_APIVERSION']
-        self.module_generation_id = os.environ['IOTEDGE_MODULEGENERATIONID']
-        self.workload_uri = os.environ['IOTEDGE_WORKLOADURI']
+        self.module_id = os.environ["IOTEDGE_MODULEID"]
+        self.api_version = os.environ["IOTEDGE_APIVERSION"]
+        self.module_generation_id = os.environ["IOTEDGE_MODULEGENERATIONID"]
+        self.workload_uri = os.environ["IOTEDGE_WORKLOADURI"]
 
     def get_trust_bundle(self):
         """
         Return the trust bundle that can be used to validate the server-side SSL
         TLS connection that we use to talk to edgeHub.
         """
-        r = requests.get(self.workload_uri + 'trust-bundle', params={'api-version': self.api_version})
+        r = requests.get(
+            self.workload_uri + "trust-bundle", params={"api-version": self.api_version}
+        )
         r.raise_for_status()
-        return r.json()['certificate']
+        return r.json()["certificate"]
 
     def sign(self, message):
         """
@@ -36,14 +38,24 @@ class IotEdgeHsm(object):
 
         returns The signature string.
         """
-        path = self.workload_uri + 'modules/' + urllib.parse.quote(self.module_id) + '/genid/' + self.module_generation_id + '/sign'
+        path = (
+            self.workload_uri
+            + "modules/"
+            + urllib.parse.quote(self.module_id)
+            + "/genid/"
+            + self.module_generation_id
+            + "/sign"
+        )
         sign_request = {
-            'keyId': 'primary',
-            'algo': 'HMACSHA256',
-            'data': base64.b64encode(message.encode()).decode()
+            "keyId": "primary",
+            "algo": "HMACSHA256",
+            "data": base64.b64encode(message.encode()).decode(),
         }
 
-        r = requests.post(path, params={'api-version': self.api_version}, data=json.dumps(sign_request))
+        r = requests.post(
+            path,
+            params={"api-version": self.api_version},
+            data=json.dumps(sign_request),
+        )
         r.raise_for_status()
-        return base64.b64decode(r.json()['digest']).decode()
-
+        return base64.b64decode(r.json()["digest"]).decode()
--- azure-iot-hub-devicesdk/tests/test_iotedge_hsm.py
+++ azure-iot-hub-devicesdk/tests/test_iotedge_hsm.py
@@ -17,24 +17,26 @@ from six.moves import mock
 from mock import MagicMock
 from mock import patch
 
-fake_module_id = '__FAKE_MODULE__ID__'
-fake_api_version = '__FAKE_API_VERSION__'
-fake_module_generation_id = '__FAKE_MODULE_GENERATION_ID__'
-fake_workload_uri = '__FAKE_WORKLOAD_URI__'
-fake_certificate = '__FAKE_CERTIFICATE__'
-fake_message = '__FAKE_MESSAGE__'
-fake_digest = '__FAKE_DIGEST__'
+fake_module_id = "__FAKE_MODULE__ID__"
+fake_api_version = "__FAKE_API_VERSION__"
+fake_module_generation_id = "__FAKE_MODULE_GENERATION_ID__"
+fake_workload_uri = "__FAKE_WORKLOAD_URI__"
+fake_certificate = "__FAKE_CERTIFICATE__"
+fake_message = "__FAKE_MESSAGE__"
+fake_digest = "__FAKE_DIGEST__"
 
 required_environment_variables = {
-    'IOTEDGE_MODULEID': fake_module_id, 
-    'IOTEDGE_APIVERSION': fake_api_version,
-    'IOTEDGE_MODULEGENERATIONID': fake_module_generation_id,
-    'IOTEDGE_WORKLOADURI': fake_workload_uri
+    "IOTEDGE_MODULEID": fake_module_id,
+    "IOTEDGE_APIVERSION": fake_api_version,
+    "IOTEDGE_MODULEGENERATIONID": fake_module_generation_id,
+    "IOTEDGE_WORKLOADURI": fake_workload_uri,
 }
+
 
 @patch.dict(os.environ, required_environment_variables)
 def test_initializer_doesnt_throw_when_all_environment_variables_are_present():
-    hsm = IotEdgeHsm();
+    hsm = IotEdgeHsm()
+
 
 def test_initializer_throws_with_missing_environment_variables():
     for key in required_environment_variables:
@@ -44,44 +46,51 @@ def test_initializer_throws_with_missing_environment_variables():
             with pytest.raises(KeyError, match=key):
                 hsm = IotEdgeHsm()
 
-@patch.object(requests, 'get')
+
+@patch.object(requests, "get")
 @patch.dict(os.environ, required_environment_variables)
 def test_get_trust_bundle_returns_certificate(mock_get):
-    mock_response = mock.Mock(spec = requests.Response)
-    mock_response.json.return_value = {'certificate': fake_certificate}
+    mock_response = mock.Mock(spec=requests.Response)
+    mock_response.json.return_value = {"certificate": fake_certificate}
     mock_get.return_value = mock_response
-    
+
     hsm = IotEdgeHsm()
     cert = hsm.get_trust_bundle()
 
-    assert(cert == fake_certificate)
+    assert cert == fake_certificate
     mock_response.raise_for_status.assert_called_once()  # this verifies that a failed status code will throw
-    mock_get.assert_called_once_with(fake_workload_uri + 'trust-bundle', params={'api-version': fake_api_version})
+    mock_get.assert_called_once_with(
+        fake_workload_uri + "trust-bundle", params={"api-version": fake_api_version}
+    )
 
-@patch.object(requests, 'post')
+
+@patch.object(requests, "post")
 @patch.dict(os.environ, required_environment_variables)
 def test_get_trust_bundle_returns_certificate(mock_post):
-    mock_response = mock.Mock(spec = requests.Response)
-    mock_response.json.return_value = {'digest': base64.b64encode(fake_digest.encode())}
+    mock_response = mock.Mock(spec=requests.Response)
+    mock_response.json.return_value = {"digest": base64.b64encode(fake_digest.encode())}
     mock_post.return_value = mock_response
 
     hsm = IotEdgeHsm()
     digest = hsm.sign(fake_message)
 
-    assert(digest == fake_digest)
+    assert digest == fake_digest
     mock_response.raise_for_status.assert_called_once()  # this verifies that a failed status code will throw
-    fake_url = fake_workload_uri + 'modules/' + fake_module_id + '/genid/' + fake_module_generation_id + '/sign'
-    fake_data = json.dumps({
-        'keyId': 'primary',
-        'algo': 'HMACSHA256',
-        'data': base64.b64encode(fake_message.encode()).decode()
-    })
-    mock_post.assert_called_once_with(fake_url, data=fake_data, params={'api-version': fake_api_version})
-    
-
-
-
-
-
-
-
+    fake_url = (
+        fake_workload_uri
+        + "modules/"
+        + fake_module_id
+        + "/genid/"
+        + fake_module_generation_id
+        + "/sign"
+    )
+    fake_data = json.dumps(
+        {
+            "keyId": "primary",
+            "algo": "HMACSHA256",
+            "data": base64.b64encode(fake_message.encode()).decode(),
+        }
+    )
+    mock_post.assert_called_once_with(
+        fake_url, data=fake_data, params={"api-version": fake_api_version}
+    )
