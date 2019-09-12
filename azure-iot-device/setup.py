c69aacf5af322de0cfcf68ca494fd8da454faeae
Carter Tinney <catinney@microsoft.com>
2019-09-11 17:40:23 -0700

    Better comment

--- azure-iot-device/setup.py
+++ azure-iot-device/setup.py
@@ -60,7 +60,7 @@ setup(
         # https://github.com/pypa/pip/issues/988
         # ---requests dependencies---
         # requests 2.22+ does not support urllib3 1.25.0 or 1.25.1 (https://github.com/psf/requests/pull/5092)
-        # requests 2.22+ is not compatible with python34, and thus only urllib < 1.25
+        # requests 2.22+ is not compatible with python34, so we must use 2.20 which is only compat w/ urllib < 1.25
         "urllib3>1.21.1,<1.26,!=1.25.0,!=1.25.1;python_version!='3.4'",
         "urllib3>1.21.1,<1.25;python_version=='3.4'",
         # Actual project dependencies
