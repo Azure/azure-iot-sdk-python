30282940e389e3dc1ad9a5c7149e0fcd86edf730
Zoltan Varga <zolvarga@microsoft.com>
2019-09-19 15:48:43 -0700

    Service Client Registry Manager initial creation (#317)
    * Service Client Registry Manager initial creation
    
    * Added placeholder tests

---
+++ azure-iot-hub/tests/test_placeholder.py
@@ -0,0 +1,14 @@
+# --------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
+"""This module should be replaced when there are actual tests for the azure.iot.hub library"""
+
+import pytest
+
+
+@pytest.mark.describe("REPLACE THIS WITH ACTUAL TESTS")
+@pytest.mark.it("Asserts True")
+def test_placeholder():
+    assert True
