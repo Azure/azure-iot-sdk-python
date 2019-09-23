cc4e3eeb1e1ddcb41217f93eb5dc5a79b9e9c796
olivakar <oliva.tanusree@gmail.com>
2019-09-23 10:56:36 -0700

    [ISSUE] Default values for content type and content encoding (#315)

--- azure-iot-device/azure/iot/device/iothub/models/message.py
+++ azure-iot-device/azure/iot/device/iothub/models/message.py
@@ -15,7 +15,7 @@ class Message(object):
     :ivar data: The data that constitutes the payload
     :ivar custom_properties: Dictionary of custom message properties
     :ivar lock_token: Used by receiver to abandon, reject or complete the message
-    :ivar message id: A user-settlable identifier for the message used for request-reply patterns. Format: A case-sensitive string (up to 128 characters long) of ASCII 7-bit alphanumeric characters + {'-', ':', '.', '+', '%', '_', '#', '*', '?', '!', '(', ')', ',', '=', '@', ';', '$', '''}
+    :ivar message id: A user-settable identifier for the message used for request-reply patterns. Format: A case-sensitive string (up to 128 characters long) of ASCII 7-bit alphanumeric characters + {'-', ':', '.', '+', '%', '_', '#', '*', '?', '!', '(', ')', ',', '=', '@', ';', '$', '''}
     :ivar sequence_number: A number (unique per device-queue) assigned by IoT Hub to each message
     :ivar to: A destination specified for Cloud-to-Device (C2D) messages
     :ivar expiry_time_utc: Date and time of message expiration in UTC format
@@ -29,15 +29,20 @@ class Message(object):
     """
 
     def __init__(
-        self, data, message_id=None, content_encoding=None, content_type=None, output_name=None
+        self,
+        data,
+        message_id=None,
+        content_encoding="utf-8",
+        content_type="application/json",
+        output_name=None,
     ):
         """
         Initializer for Message
 
         :param data: The  data that constitutes the payload
         :param str message_id: A user-settable identifier for the message used for request-reply patterns. Format: A case-sensitive string (up to 128 characters long) of ASCII 7-bit alphanumeric characters + {'-', ':', '.', '+', '%', '_', '#', '*', '?', '!', '(', ')', ',', '=', '@', ';', '$', '''}
-        :param str content_encoding: Content encoding of the message data. Can be 'utf-8', 'utf-16' or 'utf-32'
-        :param str content_type: Content type property used to routes with the message body. Can be 'application/json'
+        :param str content_encoding: Content encoding of the message data. Default is 'utf-8'. Other values can be utf-16' or 'utf-32'
+        :param str content_type: Content type property used to routes with the message body. Default value is 'application/json'
         :param str output_name: Name of the output that the is being sent to.
         """
         self.data = data
