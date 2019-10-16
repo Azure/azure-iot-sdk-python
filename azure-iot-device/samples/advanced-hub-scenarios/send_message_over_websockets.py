e609ffa480b4aaff67aade661f8a7e7de7ae3f45
Yoseph Maguire <yoseph.maguire@microsoft.com>
2019-10-16 11:40:30 -0700

    add sample

---
+++ azure-iot-device/samples/advanced-hub-scenarios/send_message_over_websockets.py
@@ -0,0 +1,37 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
+
+import os
+import asyncio
+from azure.iot.device.aio import IoTHubDeviceClient
+
+
+async def main():
+    # Fetch the connection string from an enviornment variable
+    conn_str = os.getenv("IOTHUB_DEVICE_CONNECTION_STRING")
+
+    # Create instance of the device client using the connection string
+    device_client = IoTHubDeviceClient.create_from_connection_string(conn_str, websockets=True)
+
+    # Connect the device client.
+    await device_client.connect()
+
+    # Send a single message
+    print("Sending message...")
+    await device_client.send_message("This is a message that is being sent")
+    print("Message successfully sent!")
+
+    # finally, disconnect
+    await device_client.disconnect()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
+    # If using Python 3.6 or below, use the following code instead of asyncio.run(main()):
+    # loop = asyncio.get_event_loop()
+    # loop.run_until_complete(main())
+    # loop.close()
