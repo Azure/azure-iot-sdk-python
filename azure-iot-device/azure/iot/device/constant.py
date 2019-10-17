88d84b14c24c3b8c5ba9c2470a77f269138af923
Carter Tinney <carter.tinney@microsoft.com>
2019-10-17 11:21:44 -0700

    release(azure-iot-device): 2.0.0-preview.12 (#357)

--- azure-iot-device/azure/iot/device/constant.py
+++ azure-iot-device/azure/iot/device/constant.py
@@ -6,7 +6,7 @@
 """This module defines constants for use across the azure-iot-device package
 """
 
-VERSION = "2.0.0-preview.11"
+VERSION = "2.0.0-preview.12"
 USER_AGENT = "py-azure-iot-device/{version}".format(version=VERSION)
 IOTHUB_API_VERSION = "2018-06-30"
 PROVISIONING_API_VERSION = "2019-03-31"
