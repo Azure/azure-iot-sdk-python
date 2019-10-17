44dbacc03e42e8c89a1929b068a23c5007d53d3e
Carter Tinney <catinney@microsoft.com>
2019-10-17 11:46:52 -0700

    Updated setup.py to show Python 3.8 compat

--- azure-iot-hub/setup.py
+++ azure-iot-hub/setup.py
@@ -54,6 +54,7 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     install_requires=["msrest"],
     python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3*, <4",
