4f28bf6a9bd9ab812c36847409aec3870d69b552
zolvarga <zolvarga@microsoft.com>
2019-11-07 10:31:20 -0800

    Clean-up provisioning

--- azure-iot-provisioning/azure/iot/provisioning/constant.py
+++
@@ -1,9 +0,0 @@
-# -------------------------------------------------------------------------
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License. See License.txt in the project root for
-# license information.
-# --------------------------------------------------------------------------
-"""This module defines constants for use across the azure-iot-provisioning package
-"""
-
-VERSION = "2.0.0-preview.1"
