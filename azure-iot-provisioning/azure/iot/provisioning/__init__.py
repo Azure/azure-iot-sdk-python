9194b0dad52044d7af3c3a5b28b3006a6f519a6e
Zoltan Varga <zolvarga@microsoft.com>
2019-11-07 10:53:16 -0800

    Clean-up provisioning (#387)

--- azure-iot-provisioning/azure/iot/provisioning/__init__.py
+++
@@ -1,8 +0,0 @@
-""" Azure IoT Provisioning Service Library
-
-This library provides service clients and associated models for communicating with Azure IoT Provisioning Service.
-"""
-
-from .iot_provisioning_service_client import IoTProvisioningServiceClient
-
-__all__ = ["IoTProvisioningServiceClient"]
