33086e6f9f9dbe462030da795314f711bcbc26eb
Yoseph Maguire <yoseph.maguire@microsoft.com>
2019-11-19 10:49:23 -0800

    bert feedback

--- azure-iot-device/azure/iot/device/common/pipeline/pipeline_stages_http.py
+++ azure-iot-device/azure/iot/device/common/pipeline/pipeline_stages_http.py
@@ -53,40 +53,13 @@ class HTTPTransportStage(PipelineStage):
             # all of its properties.
             logger.debug("{}({}): got connection args".format(self.name, op.name))
             self.hostname = op.hostname
-            self.username = op.username
-            self.client_id = op.client_id
             self.ca_cert = op.ca_cert
             self.sas_token = op.sas_token
             self.client_cert = op.client_cert
             self.transport = HTTPTransport(
                 hostname=self.hostname, ca_cert=self.ca_cert, x509_cert=self.client_cert
             )
-            self.transport.on_http_connected_handler = CallableWeakMethod(
-                self, "_on_http_connected"
-            )
-            self.transport.on_http_connection_failure_handler = CallableWeakMethod(
-                self, "_on_http_connection_failure"
-            )
-            self.transport.on_http_disconnected_handler = CallableWeakMethod(
-                self, "_on_http_disconnected"
-            )
-            self.transport.on_http_message_received_handler = CallableWeakMethod(
-                self, "_on_http_message_received"
-            )
 
-            # There can only be one pending connection operation (Connect, Reconnect, Disconnect)
-            # at a time. The existing one must be completed or canceled before a new one is set.
-
-            # Currently, this means that if, say, a connect operation is the pending op and is executed
-            # but another connection op is begins by the time the CONNACK is received, the original
-            # operation will be cancelled, but the CONNACK for it will still be received, and complete the
-            # NEW operation. This is not desirable, but it is how things currently work.
-
-            # We are however, checking the type, so the CONNACK from a cancelled Connect, cannot successfully
-            # complete a Disconnect operation.
-            self._pending_connection_op = None
-
-            self.pipeline_root.transport = self.transport
             self.complete_op(op)
 
         elif isinstance(op, pipeline_ops_base.UpdateSasTokenOperation):
@@ -94,170 +67,10 @@ class HTTPTransportStage(PipelineStage):
             self.sas_token = op.sas_token
             self.complete_op(op)
 
-        elif isinstance(op, pipeline_ops_base.ConnectOperation):
-            logger.info("{}({}): connecting".format(self.name, op.name))
-
-            self._cancel_pending_connection_op()
-            self._pending_connection_op = op
-            try:
-                self.transport.connect(password=self.sas_token)
-            except Exception as e:
-                logger.error("transport.connect raised error")
-                logger.error(traceback.format_exc())
-                self._pending_connection_op = None
-                self.complete_op(op, error=e)
-
-        elif isinstance(op, pipeline_ops_base.ReconnectOperation):
-            logger.info("{}({}): reconnecting".format(self.name, op.name))
-
-            # We set _active_connect_op here because a reconnect is the same as a connect for "active operation" tracking purposes.
-            self._cancel_pending_connection_op()
-            self._pending_connection_op = op
-            try:
-                self.transport.reconnect(password=self.sas_token)
-            except Exception as e:
-                logger.error("transport.reconnect raised error")
-                logger.error(traceback.format_exc())
-                self._pending_connection_op = None
-                self.complete_op(op, error=e)
-
-        elif isinstance(op, pipeline_ops_base.DisconnectOperation):
-            logger.info("{}({}): disconnecting".format(self.name, op.name))
-
-            self._cancel_pending_connection_op()
-            self._pending_connection_op = op
-            try:
-                self.transport.disconnect()
-            except Exception as e:
-                logger.error("transport.disconnect raised error")
-                logger.error(traceback.format_exc())
-                self._pending_connection_op = None
-                self.complete_op(op, error=e)
-
-        elif isinstance(op, pipeline_ops_http.MQTTPublishOperation):
-            logger.info("{}({}): publishing on {}".format(self.name, op.name, op.topic))
-
-            @pipeline_thread.invoke_on_pipeline_thread_nowait
-            def on_published():
-                logger.debug("{}({}): PUBACK received. completing op.".format(self.name, op.name))
-                self.complete_op(op)
-
-            self.transport.publish(topic=op.topic, payload=op.payload, callback=on_published)
-
-        elif isinstance(op, pipeline_ops_http.MQTTSubscribeOperation):
-            logger.info("{}({}): subscribing to {}".format(self.name, op.name, op.topic))
-
-            @pipeline_thread.invoke_on_pipeline_thread_nowait
-            def on_subscribed():
-                logger.debug("{}({}): SUBACK received. completing op.".format(self.name, op.name))
-                self.complete_op(op)
-
-            self.transport.subscribe(topic=op.topic, callback=on_subscribed)
-
-        elif isinstance(op, pipeline_ops_http.MQTTUnsubscribeOperation):
-            logger.info("{}({}): unsubscribing from {}".format(self.name, op.name, op.topic))
-
-            @pipeline_thread.invoke_on_pipeline_thread_nowait
-            def on_unsubscribed():
-                logger.debug(
-                    "{}({}): UNSUBACK received.  completing op.".format(self.name, op.name)
-                )
-                self.complete_op(op)
-
-            self.transport.unsubscribe(topic=op.topic, callback=on_unsubscribed)
+        elif isinstance(op, pipeline_ops_http.HTTPRequestOperation):
+            # TODO: IMPLEMENT THIS!!!
+            logger.info("{}({}): Handling HTTP Request Operation".format(self.name, op.name))
+            self.complete_op(op)
 
         else:
             self.send_op_down(op)
-
-    @pipeline_thread.invoke_on_pipeline_thread_nowait
-    def _on_http_connected(self):
-        """
-        Handler that gets called by the transport when it connects.
-        """
-        logger.info("_on_http_connected called")
-        # self.on_connected() tells other pipeline stages that we're connected.  Do this before
-        # we do anything else (in case upper stages have any "are we connected" logic.
-        self.on_connected()
-
-        if isinstance(
-            self._pending_connection_op, pipeline_ops_base.ConnectOperation
-        ) or isinstance(self._pending_connection_op, pipeline_ops_base.ReconnectOperation):
-            logger.debug("completing connect op")
-            op = self._pending_connection_op
-            self._pending_connection_op = None
-            self.complete_op(op)
-        else:
-            # This should indicate something odd is going on.
-            # If this occurs, either a connect was completed while there was no pending op,
-            # OR that a connect was completed while a disconnect op was pending
-            logger.warning("Connection was unexpected")
-
-    @pipeline_thread.invoke_on_pipeline_thread_nowait
-    def _on_http_connection_failure(self, cause):
-        """
-        Handler that gets called by the transport when a connection fails.
-
-        :param Exception cause: The Exception that caused the connection failure.
-        """
-
-        logger.error("{}: _on_http_connection_failure called: {}".format(self.name, cause))
-
-        if isinstance(
-            self._pending_connection_op, pipeline_ops_base.ConnectOperation
-        ) or isinstance(self._pending_connection_op, pipeline_ops_base.ReconnectOperation):
-            logger.debug("{}: failing connect op".format(self.name))
-            op = self._pending_connection_op
-            self._pending_connection_op = None
-            self.complete_op(op, error=cause)
-        else:
-            logger.warning("{}: Connection failure was unexpected".format(self.name))
-            handle_exceptions.handle_background_exception(cause)
-
-    @pipeline_thread.invoke_on_pipeline_thread_nowait
-    def _on_http_disconnected(self, cause=None):
-        """
-        Handler that gets called by the transport when the transport disconnects.
-
-        :param Exception cause: The Exception that caused the disconnection, if any (optional)
-        """
-        if cause:
-            logger.error("{}: _on_http_disconnect called: {}".format(self.name, cause))
-        else:
-            logger.info("{}: _on_http_disconnect called".format(self.name))
-
-        # self.on_disconnected() tells other pipeilne stages that we're disconnected.  Do this before
-        # we do anything else (in case upper stages have any "are we connected" logic.
-        self.on_disconnected()
-
-        if self._pending_connection_op:
-            # on_http_disconnected will cause any pending connect op to complete.  This is how Paho
-            # behaves when there is a connection error, and it also makes sense that on_http_disconnected
-            # would cause a pending connection op to fail.
-            logger.debug(
-                "{}: completing pending {} op".format(self.name, self._pending_connection_op.name)
-            )
-            op = self._pending_connection_op
-            self._pending_connection_op = None
-
-            if isinstance(op, pipeline_ops_base.DisconnectOperation):
-                # Swallow any errors if we intended to disconnect - even if something went wrong, we
-                # got to the state we wanted to be in!
-                if cause:
-                    handle_exceptions.swallow_unraised_exception(
-                        cause,
-                        log_msg="Unexpected disconnect with error while disconnecting - swallowing error",
-                    )
-                self.complete_op(op)
-            else:
-                if cause:
-                    self.complete_op(op, error=cause)
-                else:
-                    self.complete_op(
-                        op,
-                        error=transport_exceptions.ConnectionDroppedError("transport disconnected"),
-                    )
-        else:
-            logger.warning("{}: disconnection was unexpected".format(self.name))
-            # Regardless of cause, it is now a ConnectionDroppedError
-            e = transport_exceptions.ConnectionDroppedError(cause=cause)
-            handle_exceptions.handle_background_exception(e)
--- azure-iot-device/azure/iot/device/iothub/pipeline/pipeline_stages_edgehub_http.py
+++ azure-iot-device/azure/iot/device/iothub/pipeline/pipeline_stages_edgehub_http.py
@@ -16,7 +16,7 @@ from azure.iot.device.common.pipeline import (
     pipeline_thread,
 )
 from azure.iot.device.edgehub.models import Message, MethodRequest
-from . import pipeline_ops_edgehub, pipeline_events_edgehub, http_path_edgehub
+from . import pipeline_ops_iothub, pipeline_ops_edgehub, http_path_edgehub
 from . import constant as pipeline_constant
 from . import exceptions as pipeline_exceptions
 from azure.iot.device import constant as pkg_constant
@@ -37,7 +37,7 @@ class EdgeHubHTTPTranslationStage(PipelineStage):
     @pipeline_thread.runs_on_pipeline_thread
     def _execute_op(self, op):
 
-        if isinstance(op, pipeline_ops_edgehub.SetEdgeHubConnectionArgsOperation):
+        if isinstance(op, pipeline_ops_iothub.SetIoTHubConnectionArgsOperation):
             self.device_id = op.device_id
             self.module_id = op.module_id
 
@@ -47,6 +47,8 @@ class EdgeHubHTTPTranslationStage(PipelineStage):
                 client_id = "{}/{}".format(op.device_id, op.module_id)
             else:
                 client_id = op.device_id
+            # When you are connecting through Edge Hub to another Hub, Gateway Hostname is the gateway you are connecting to, hostname is the IoT Hub you are connecting to.
+            # TODO: Read the node code to figure out how to format the HTTP url with the gateway hostname and the hostname
             if op.gateway_hostname:
                 hostname = op.gateway_hostname
             else:
@@ -66,10 +68,8 @@ class EdgeHubHTTPTranslationStage(PipelineStage):
                 op=op,
             )
 
-        elif (
-            isinstance(op, pipeline_ops_edgehub.MethodInvokeOperation)
-            # and self.pipeline_root.connected # TODO: See if this is necessary or not.
-        ):
+        elif isinstance(op, pipeline_ops_edgehub.MethodInvokeOperation):
+            # TODO: translate the method params into the HTTP specific operations. It sets the path, the header values, picks the verb (METHOD INVOKE is a POST)
             logger.debug(
                 "{}({}): Connected.  Passing op down and reconnecting after token is updated.".format(
                     self.name, op.name
@@ -85,18 +85,6 @@ class EdgeHubHTTPTranslationStage(PipelineStage):
             )
             self.send_op_down(op)
 
-        elif isinstance(op, pipeline_ops_edgehub.SendD2CMessageOperation) or isinstance(
-            op, pipeline_ops_edgehub.SendOutputEventOperation
-        ):
-            # Convert Get Storage Info operation into HTTP Publish operations
-            path = http_path_edgehub.encode_properties(op.message, self.telemetry_topic)
-            self.send_worker_op_down(
-                worker_op=pipeline_ops_http.HTTPPublishOperation(
-                    path=path, payload=op.message.data, callback=op.callback
-                ),
-                op=op,
-            )
-
         else:
             # All other operations get passed down
             self.send_op_down(op)
@@ -109,59 +97,3 @@ class EdgeHubHTTPTranslationStage(PipelineStage):
         self.telemetry_topic = http_path_edgehub.get_telemetry_topic_for_publish(
             device_id, module_id
         )
-
-
-# FOR NOW, I DON'T THINK THIS IS NECESSARY
-# @pipeline_thread.runs_on_pipeline_thread
-# def _handle_pipeline_event(self, event):
-#     """
-#     Pipeline Event handler function to convert incoming HTTP messages into the appropriate EdgeHub
-#     events, based on the topic of the message
-#     """
-#     if isinstance(event, pipeline_events_http.IncomingHTTPMessageEvent):
-#         topic = event.topic
-
-#         if http_path_edgehub.is_c2d_topic(topic, self.device_id):
-#             message = Message(event.payload)
-#             http_path_edgehub.extract_properties_from_topic(topic, message)
-#             self.send_event_up(pipeline_events_edgehub.C2DMessageEvent(message))
-
-#         elif http_path_edgehub.is_input_topic(topic, self.device_id, self.module_id):
-#             message = Message(event.payload)
-#             http_path_edgehub.extract_properties_from_topic(topic, message)
-#             input_name = http_path_edgehub.get_input_name_from_topic(topic)
-#             self.send_event_up(pipeline_events_edgehub.InputMessageEvent(input_name, message))
-
-#         elif http_path_edgehub.is_method_topic(topic):
-#             request_id = http_path_edgehub.get_method_request_id_from_topic(topic)
-#             method_name = http_path_edgehub.get_method_name_from_topic(topic)
-#             method_received = MethodRequest(
-#                 request_id=request_id,
-#                 name=method_name,
-#                 payload=json.loads(event.payload.decode("utf-8")),
-#             )
-#             self.send_event_up(pipeline_events_edgehub.MethodRequestEvent(method_received))
-
-#         elif http_path_edgehub.is_twin_response_topic(topic):
-#             request_id = http_path_edgehub.get_twin_request_id_from_topic(topic)
-#             status_code = int(http_path_edgehub.get_twin_status_code_from_topic(topic))
-#             self.send_event_up(
-#                 pipeline_events_base.ResponseEvent(
-#                     request_id=request_id, status_code=status_code, response_body=event.payload
-#                 )
-#             )
-
-#         elif http_path_edgehub.is_twin_desired_property_patch_topic(topic):
-#             self.send_event_up(
-#                 pipeline_events_edgehub.TwinDesiredPropertiesPatchEvent(
-#                     patch=json.loads(event.payload.decode("utf-8"))
-#                 )
-#             )
-
-#         else:
-#             logger.debug("Uunknown topic: {} passing up to next handler".format(topic))
-#             self.send_event_up(event)
-
-#     else:
-#         # all other messages get passed up
-#         self.send_event_up(event)
