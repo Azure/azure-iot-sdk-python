b038f729011c5a7711019ca9e33d69573099b12c
Carter Tinney <catinney@microsoft.com>
2020-01-10 14:34:55 -0800

    transport changes

--- azure-iot-device/tests/common/test_mqtt_transport.py
+++ azure-iot-device/tests/common/test_mqtt_transport.py
@@ -230,6 +230,24 @@ class TestInstantiation(object):
             cadata=server_verification_cert
         )
 
+    # TODO: Finalize these test args once the format for ciphers is determined
+    @pytest.mark.it(
+        "Configures TLS/SSL context with provided cipher(s) if present during instantiation"
+    )
+    @pytest.mark.parametrize(
+        "cipher", [pytest.param("", id="Single cipher"), pytest.param("", id="Cipher list")]
+    )
+    def test_confgures_tls_context_with_cipher(self, mocker, mock_mqtt_client, cipher):
+        mock_ssl_context_constructor = mocker.patch.object(ssl, "SSLContext")
+        mock_ssl_context = mock_ssl_context_constructor.return_value
+
+        MQTTTransport(
+            client_id=fake_device_id, hostname=fake_hostname, username=fake_username, cipher=cipher
+        )
+
+        assert mock_ssl_context.set_ciphers.call_count == 1
+        assert mock_ssl_context.set_ciphers.call_args == mocker.call(cipher)
+
     @pytest.mark.it("Configures TLS/SSL context with client-provided-certificate-chain like x509")
     def test_configures_tls_context_with_client_provided_certificate_chain(
         self, mocker, mock_mqtt_client
