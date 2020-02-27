00f6c6348ec85b665bfc4be29e62ee3794c82029
Azure IoT Client Build <aziotclb@microsoft.com>
2020-02-27 22:50:52 +0000

    Bump package versions for release on 2020-02-27

--- azure-iot-device/azure/iot/device/constant.py
+++ azure-iot-device/azure/iot/device/constant.py
@@ -6,7 +6,7 @@
 """This module defines constants for use across the azure-iot-device package
 """
 
-VERSION = "2.0.1"
+VERSION = "2.1.0"
 IOTHUB_IDENTIFIER = "azure-iot-device-iothub-py"
 PROVISIONING_IDENTIFIER = "azure-iot-device-provisioning-py"
 IOTHUB_API_VERSION = "2018-06-30"
