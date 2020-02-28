0002d32ff6839b4e965ef63e01b3179c87a7ee5a
zolvarga <zolvarga@microsoft.com>
2020-02-28 12:47:23 -0800

    feature(azure-iot-hub): Implement htpp runtime client, add unit tests

--- azure-iot-hub/azure/iot/hub/__init__.py
+++ azure-iot-hub/azure/iot/hub/__init__.py
@@ -6,5 +6,11 @@ This library provides service clients and associated models for communicating wi
 from .iothub_registry_manager import IoTHubRegistryManager
 from .iothub_configuration_manager import IoTHubConfigurationManager
 from .iothub_job_manager import IoTHubJobManager
+from .iothub_http_runtime_manager import IoTHubHttpRuntimeManager
 
-__all__ = ["IoTHubRegistryManager", "IoTHubConfigurationManager", "IoTHubJobManager"]
+__all__ = [
+    "IoTHubRegistryManager",
+    "IoTHubConfigurationManager",
+    "IoTHubJobManager",
+    "IoTHubHttpRuntimeManager",
+]
