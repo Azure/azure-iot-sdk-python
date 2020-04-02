f1fbc1eb498dc3b90bafb3ca2e8eda525b73b6fe
Yoseph Maguire <yoseph.maguire@microsoft.com>
2020-04-02 11:10:41 -0700

    fix: upload_to_blob

--- azure-iot-device/samples/async-hub-scenarios/upload_to_blob.py
+++ azure-iot-device/samples/async-hub-scenarios/upload_to_blob.py
@@ -13,16 +13,17 @@ import http.client
 import pprint
 import json
 from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
+from azure.core.exceptions import ResourceExistsError
 import logging
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 """
-Welcome to the Upload to Blob sample. To use this sample you must have azure.storage.blob installed in your python environment.
+Welcome to the Upload to Blob sample for the Azure IoT Python SDK. To use this sample you must have azure.storage.blob installed in your python environment.
 To do this, you can run:
 
-    $ pip isntall azure.storage.blob
+    $ pip install azure.storage.blob
 
 This sample covers using the following Device Client APIs:
 
@@ -42,76 +43,105 @@ https://docs.microsoft.com/en-us/azure/iot-hub/iot-hub-devguide-file-upload
 
 """
 
+IOTHUB_HOSTNAME = os.getenv("IOTHUB_HOSTNAME")
+IOTHUB_DEVICE_ID = os.getenv("IOTHUB_DEVICE_ID")
+
+X509_CERT_FILE = os.getenv("X509_CERT_FILE")
+X509_KEY_FILE = os.getenv("X509_KEY_FILE")
+X509_PASS_PHRASE = os.getenv("PASS_PHRASE")
+
 # Host is in format "<iothub name>.azure-devices.net"
 
 
-async def storage_blob(blob_info):
-    try:
-        print("Azure Blob storage v12 - Python quickstart sample")
-        sas_url = "https://{}/{}/{}{}".format(
-            blob_info["hostName"],
-            blob_info["containerName"],
-            blob_info["blobName"],
-            blob_info["sasToken"],
-        )
-        blob_client = BlobClient.from_blob_url(sas_url)
-        # Create a file in local Documents directory to upload and download
-        local_file_name = "data/quickstart" + str(uuid.uuid4()) + ".txt"
-        filename = os.path.join(os.path.dirname(os.path.realpath(__file__)), local_file_name)
-        # Write text to the file
-        if not os.path.exists(os.path.dirname(filename)):
-            os.makedirs(os.path.dirname(filename))
-        file = open(filename, "w")
-        file.write("Hello, World!")
-        file.close()
+async def upload_via_storage_blob(blob_info):
+    """ Helper function written to perform Storage Blob V12 Upload Tasks
 
-        print("\nUploading to Azure Storage as blob:\n\t" + local_file_name)
-        # # Upload the created file
-        with open(filename, "rb") as f:
-            result = blob_client.upload_blob(f)
-            return (None, result)
+    Arguments:
+    blob_info - an object containing the information needed to generate a sas_url for creating a blob client
 
-    except Exception as ex:
-        print("Exception:")
-        print(ex)
-        return ex
+    Returns:
+    status of blob upload operation, in the storage provided strcuture.
+    """
+
+    print("Azure Blob storage v12 - Python quickstart sample")
+    sas_url = "https://{}/{}/{}{}".format(
+        blob_info["hostName"],
+        blob_info["containerName"],
+        blob_info["blobName"],
+        blob_info["sasToken"],
+    )
+    blob_client = BlobClient.from_blob_url(sas_url)
+
+    # The following file code can be replaced with simply a sample file in a directory.
+
+    # Create a file in local Documents directory to upload and download
+    local_file_name = "data/quickstart" + str(uuid.uuid4()) + ".txt"
+    filename = os.path.join(os.path.dirname(os.path.realpath(__file__)), local_file_name)
+
+    # Write text to the file
+    if not os.path.exists(os.path.dirname(filename)):
+        os.makedirs(os.path.dirname(filename))
+    file = open(filename, "w")
+    file.write("Hello, World!")
+    file.close()
+
+    # Perform the actual upload for the data.
+    print("\nUploading to Azure Storage as blob:\n\t" + local_file_name)
+    # # Upload the created file
+    with open(filename, "rb") as data:
+        result = blob_client.upload_blob(data)
+
+    return result
 
 
 async def main():
-    hostname = os.getenv("IOTHUB_HOSTNAME")
-    device_id = os.getenv("IOTHUB_DEVICE_ID")
-    x509 = X509(
-        cert_file=os.getenv("X509_CERT_FILE"),
-        key_file=os.getenv("X509_KEY_FILE"),
-        pass_phrase=os.getenv("PASS_PHRASE"),
-    )
+    hostname = IOTHUB_HOSTNAME
+    device_id = IOTHUB_DEVICE_ID
+    x509 = X509(cert_file=X509_CERT_FILE, key_file=X509_KEY_FILE, pass_phrase=X509_PASS_PHRASE)
 
+    # Create the Device Client.
     device_client = IoTHubDeviceClient.create_from_x509_certificate(
         hostname=hostname, device_id=device_id, x509=x509
     )
-    # device_client = IoTHubModuleClient.create_from_connection_string(conn_str)
 
     # Connect the client.
     await device_client.connect()
 
-    # await device_client.get_storage_info_for_blob("fake_device", "fake_method_params")
-
-    # get the storage sas
+    # get the Storage SAS information from IoT Hub.
     blob_name = "fakeBlobName12"
     storage_info = await device_client.get_storage_info_for_blob(blob_name)
+    result = {"status_code": -1, "status_description": "N/A"}
 
-    # upload to blob
-    connection = http.client.HTTPSConnection(hostname)
-    connection.connect()
-    # notify iot hub of blob upload result
-    # await device_client.notify_upload_result(storage_blob_result)
-    storage_blob_result = await storage_blob(storage_info)
+    # Using the Storage Blob V12 API, perform the blob upload.
+    try:
+        upload_result = await upload_via_storage_blob(storage_info)
+        if upload_result.error_code:
+            result = {
+                "status_code": upload_result.error_code,
+                "status_description": "Storage Blob Upload Error",
+            }
+        else:
+            result = {"status_code": 200, "status_description": ""}
+    except ResourceExistsError as ex:
+        if ex.status_code:
+            result = {"status_code": ex.status_code, "status_description": ex.reason}
+        else:
+            print("Failed with Exception: {}", ex)
+            result = {"status_code": 400, "status_description": ex.message}
+
     pp = pprint.PrettyPrinter(indent=4)
-    pp.pprint(storage_blob_result)
-    connection.close()
-    await device_client.notify_blob_upload_status(
-        storage_info["correlationId"], True, 200, "fake status description"
-    )
+    pp.pprint(result)
+    if result["status_code"] == 200:
+        await device_client.notify_blob_upload_status(
+            storage_info["correlationId"], True, result["status_code"], result["status_description"]
+        )
+    else:
+        await device_client.notify_blob_upload_status(
+            storage_info["correlationId"],
+            False,
+            result["status_code"],
+            result["status_description"],
+        )
 
     # Finally, disconnect
     await device_client.disconnect()
