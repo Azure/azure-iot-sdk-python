10c0ce4a769562a807859d4fcbf066df8bc1d3ae
Elena Horton <52430760+elhorton@users.noreply.github.com>
2020-09-24 00:13:04 -0700

    Update get_digital_twin_sample.py

--- azure-iot-hub/samples/get_digital_twin_sample.py
+++ azure-iot-hub/samples/get_digital_twin_sample.py
@@ -7,7 +7,7 @@
 import sys
 import os
 import msrest
-from azure.iot.hub import IoTHubDigitalTwinManager, IoTHubRegistryManager
+from azure.iot.hub import IoTHubDigitalTwinManager
 
 
 iothub_connection_str = os.getenv("IOTHUB_CONNECTION_STRING")
@@ -24,16 +24,6 @@ try:
         print("Model Id: " + digital_twin["$metadata"]["$model"])
     else:
         print("No digital_twin found")
-
-    iothub_registry_manager = IoTHubRegistryManager(iothub_connection_str)
-    twin = iothub_registry_manager.get_twin(device_id)
-    print("Full Twin is:")
-    print(twin)
-
-    additional_props = twin.additional_properties
-    if "modelId" in additional_props:
-        print("Model id for digital twin is")
-        print("ModelId:" + additional_props["modelId"])
 
 
 except msrest.exceptions.HttpOperationError as ex:
