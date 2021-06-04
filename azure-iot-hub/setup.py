8e5e7aba08264cdd7de86d8b18e25a5caf6e29e0
Vishnu Reddy <vishnureddy@microsoft.com>
2021-06-03 23:36:18 -0700

    add azure.identity dependency

--- azure-iot-hub/setup.py
+++ azure-iot-hub/setup.py
@@ -72,7 +72,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
     ],
-    install_requires=["msrest", "uamqp", "azure.core"],
+    install_requires=["msrest", "uamqp", "azure.core", "azure.identity"],
     python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3*, <4",
     packages=find_packages(
         exclude=[
