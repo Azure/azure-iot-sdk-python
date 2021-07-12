0eb6d530b24b9fd922186fc9ac389b726178a8f1
Bert Kleewein <BertKleewein@users.noreply.github.com>
2021-07-12 12:52:07 -0700

    Property interfaces renames, Command renamed to CommandRequest (#819)

--- azure-iot-device/azure/iot/device/iothub/models/__init__.py
+++ azure-iot-device/azure/iot/device/iothub/models/__init__.py
@@ -5,5 +5,5 @@ This package provides object models for use within the Azure IoT Hub Device SDK.
 
 from .message import Message
 from .methods import MethodRequest, MethodResponse
-from .commands import Command, CommandResponse
+from .commands import CommandRequest, CommandResponse
 from .properties import ClientProperties, ClientPropertyCollection, WritablePropertyResponse
--- azure-iot-device/tests/iothub/models/test_commands.py
+++ azure-iot-device/tests/iothub/models/test_commands.py
@@ -4,7 +4,7 @@
 # license information.
 # --------------------------------------------------------------------------
 
-from azure.iot.device.iothub.models.commands import Command, CommandResponse
+from azure.iot.device.iothub.models.commands import CommandRequest, CommandResponse
 import pytest
 import logging
 
@@ -17,75 +17,75 @@ dummy_payload = {"key": "value"}
 dummy_status = 200
 
 
-@pytest.mark.describe("Command - Instantiation")
+@pytest.mark.describe("CommandRequest - Instantiation")
 class TestCommandInstantiation(object):
     @pytest.mark.it(
         "Instantiates with a read-only 'request_id' attribute set to the value of the provided 'request_id' parameter"
     )
     def test_request_id(self):
-        command = Command(
+        command_request = CommandRequest(
             request_id=dummy_rid,
             component_name=dummy_component_name,
             command_name=dummy_command_name,
             payload=dummy_payload,
         )
 
-        assert command.request_id == dummy_rid
+        assert command_request.request_id == dummy_rid
 
         new_rid = 2
         with pytest.raises(AttributeError):
-            command.request_id = new_rid
+            command_request.request_id = new_rid
 
     @pytest.mark.it(
         "Instantiates with a read-only 'component_name' attribute set to the value of the provided 'component_name' parameter"
     )
     def test_component_name(self):
-        command = Command(
+        command_request = CommandRequest(
             request_id=dummy_rid,
             component_name=dummy_component_name,
             command_name=dummy_command_name,
             payload=dummy_payload,
         )
 
-        assert command.component_name == dummy_component_name
+        assert command_request.component_name == dummy_component_name
 
         new_name = "new_name"
         with pytest.raises(AttributeError):
-            command.component_name = new_name
+            command_request.component_name = new_name
 
     @pytest.mark.it(
         "Instantiates with a read-only 'command_name' attribute set to the value of the provided 'command_name' parameter"
     )
     def test_command_name(self):
-        command = Command(
+        command_request = CommandRequest(
             request_id=dummy_rid,
             component_name=dummy_component_name,
             command_name=dummy_command_name,
             payload=dummy_payload,
         )
 
-        assert command.command_name == dummy_command_name
+        assert command_request.command_name == dummy_command_name
 
         new_name = "new_name"
         with pytest.raises(AttributeError):
-            command.command_name = new_name
+            command_request.command_name = new_name
 
     @pytest.mark.it(
         "Instantiates with a read-only 'payload' attribute set to the value of the provided 'payload' parameter"
     )
     def test_payload(self):
-        command = Command(
+        command_request = CommandRequest(
             request_id=dummy_rid,
             component_name=dummy_component_name,
             command_name=dummy_command_name,
             payload=dummy_payload,
         )
 
-        assert command.payload == dummy_payload
+        assert command_request.payload == dummy_payload
 
         new_payload = {"another_key": "another_value"}
         with pytest.raises(AttributeError):
-            command.payload = new_payload
+            command_request.payload = new_payload
 
 
 @pytest.mark.describe("CommandResponse - Instantiation")
@@ -125,11 +125,11 @@ class TestCommandResponseInstantiation(object):
         assert command_response.payload is None
 
 
-@pytest.mark.describe("CommandResponse - .create_from_command()")
+@pytest.mark.describe("CommandResponse - .create_from_command_request()")
 class TestCommandResponseCreateFromCommand(object):
     @pytest.fixture
-    def command(self):
-        return Command(
+    def command_request(self):
+        return CommandRequest(
             request_id=dummy_rid,
             component_name=dummy_component_name,
             command_name=dummy_command_name,
@@ -137,24 +137,26 @@ class TestCommandResponseCreateFromCommand(object):
         )
 
     @pytest.mark.it(
-        "Creates and returns a CommandResponse object containing the provided status and payload, using the request_id of the provided Command"
+        "Creates and returns a CommandResponse object containing the provided status and payload, using the request_id of the provided CommandRequest"
     )
-    def test_instantiation(self, command):
+    def test_instantiation(self, command_request):
         payload = {"another_key": "another_value"}
         status = 400
-        command_response = CommandResponse.create_from_command(
-            command=command, status=status, payload=payload
+        command_response = CommandResponse.create_from_command_request(
+            command_request=command_request, status=status, payload=payload
         )
 
-        assert command_response.request_id == command.request_id
+        assert command_response.request_id == command_request.request_id
         assert command_response.status == status
         assert command_response.payload == payload
 
     @pytest.mark.it(
         "Defaults the value of 'payload' to None if no value for the 'payload' parameter is provided"
     )
-    def test_default_payload(self, command):
+    def test_default_payload(self, command_request):
         status = 400
-        command_response = CommandResponse.create_from_command(command=command, status=status)
+        command_response = CommandResponse.create_from_command_request(
+            command_request=command_request, status=status
+        )
 
         assert command_response.payload is None
