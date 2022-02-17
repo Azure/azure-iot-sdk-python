36c331dccfc62532f7541d1e8cd1ea6d459d375e
Bert Kleewein <bertk@microsoft.com>
2022-02-17 11:51:59 -0800

    fix tests

--- azure-iot-hub/tests/test_iothub_amqp_client.py
+++ azure-iot-hub/tests/test_iothub_amqp_client.py
@@ -154,7 +154,7 @@ class TestIoTHubAmqpClientSharedAccessKeyAuthInstantiation(
         assert mock_uamqp_SendClient.call_count == 1
         expected_target = "amqps://" + fake_hostname + "/messages/devicebound"
         assert mock_uamqp_SendClient.call_args == mocker.call(
-            target=expected_target, auth=amqp_token_mock
+            target=expected_target, auth=amqp_token_mock, keep_alive_interval=120
         )
 
     @pytest.mark.it("Creates an HMAC to generate a shared access signature")
@@ -236,7 +236,7 @@ class TestIotHubAmqpClientTokenAuthInstantiation(IoTHubAmqpClientTokenAuthTestCo
         assert mock_uamqp_SendClient.call_count == 1
         expected_target = "amqps://" + fake_hostname + "/messages/devicebound"
         assert mock_uamqp_SendClient.call_args == mocker.call(
-            target=expected_target, auth=amqp_token_mock
+            target=expected_target, auth=amqp_token_mock, keep_alive_interval=120
         )
 
     @pytest.mark.it(
@@ -261,7 +261,7 @@ class TestIotHubAmqpClientTokenAuthInstantiation(IoTHubAmqpClientTokenAuthTestCo
         assert mock_uamqp_SendClient.call_count == 1
         expected_target = "amqps://" + fake_hostname + "/messages/devicebound"
         assert mock_uamqp_SendClient.call_args == mocker.call(
-            target=expected_target, auth=amqp_token_mock
+            target=expected_target, auth=amqp_token_mock, keep_alive_interval=120
         )
 
     @pytest.mark.it(
