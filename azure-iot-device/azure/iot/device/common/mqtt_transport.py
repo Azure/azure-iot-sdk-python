275503effdb08e307dbe2ff0f17a6ff346ce4ed7
Carter Tinney <catinney@microsoft.com>
2022-02-18 15:15:06 -0800

    test removing weakref from transport pt2

--- azure-iot-device/azure/iot/device/common/mqtt_transport.py
+++ azure-iot-device/azure/iot/device/common/mqtt_transport.py
@@ -170,7 +170,8 @@ class MQTTTransport(object):
         # Set event handlers.  Use weak references back into this object to prevent
         # leaks on Python 2.7.  See PEP 442 for explanation.
         # self_weakref = weakref.ref(self)
-        self_weakref = self
+        def self_weakref():
+            return self
 
         def on_connect(client, userdata, flags, rc):
             this = self_weakref()
