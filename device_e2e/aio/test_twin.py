05ade35e86b2f4be711b65f260b7e4b05351c70d
Bert Kleewein <BertKleewein@users.noreply.github.com>
2022-02-22 16:22:06 -0800

    test: Rename some tests for accuracy (#954)

--- device_e2e/aio/test_twin.py
+++ device_e2e/aio/test_twin.py
@@ -22,7 +22,7 @@ reset_reported_props = {const.TEST_CONTENT: None}
 class TestReportedProperties(object):
     @pytest.mark.it("Can set a simple reported property")
     @pytest.mark.quicktest_suite
-    async def test_simple_patch(self, client, random_reported_props, service_helper):
+    async def test_sends_simple_reported_patch(self, client, random_reported_props, service_helper):
 
         # patch properties
         await client.patch_twin_reported_properties(random_reported_props)
@@ -64,7 +64,9 @@ class TestReportedProperties(object):
 
     @pytest.mark.it("Connects the transport if necessary")
     @pytest.mark.quicktest_suite
-    async def test_connect_if_necessary(self, client, random_reported_props, service_helper):
+    async def test_patch_reported_connect_if_necessary(
+        self, client, random_reported_props, service_helper
+    ):
 
         await client.disconnect()
 
@@ -89,8 +91,8 @@ class TestReportedPropertiesDroppedConnection(object):
 
     # TODO: split drop tests between first and second patches
 
-    @pytest.mark.it("Sends if connection drops before sending")
-    async def test_sends_if_drop_before_sending(
+    @pytest.mark.it("Updates reported properties if connection drops before sending")
+    async def test_updates_reported_if_drop_before_sending(
         self, client, random_reported_props, dropper, service_helper
     ):
 
@@ -117,8 +119,8 @@ class TestReportedPropertiesDroppedConnection(object):
             == random_reported_props[const.TEST_CONTENT]
         )
 
-    @pytest.mark.it("Sends if connection rejects send")
-    async def test_sends_if_reject_before_sending(
+    @pytest.mark.it("Updates reported properties if connection rejects send")
+    async def test_udpates_reported_if_reject_before_sending(
         self, client, random_reported_props, dropper, service_helper
     ):
 
@@ -150,7 +152,7 @@ class TestReportedPropertiesDroppedConnection(object):
 class TestDesiredProperties(object):
     @pytest.mark.it("Receives a patch for a simple desired property")
     @pytest.mark.quicktest_suite
-    async def test_simple_patch(self, client, event_loop, service_helper):
+    async def test_receives_simple_desired_patch(self, client, event_loop, service_helper):
 
         received_patch = None
         received = asyncio.Event()
