c91f2741366df668324b628b750af5f9c7e8e10f
Carter Tinney <catinney@microsoft.com>
2022-07-29 16:09:40 -0700

    updated build script (non-release)

--- scripts/build_packages.py
+++ scripts/build_packages.py
@@ -5,16 +5,13 @@
 # --------------------------------------------------------------------------
 """Build all packages for distribution"""
 
-import glob
-import os
+
 import sys
 from subprocess import check_call
 
 if __name__ == "__main__":
-    target_dir = "../dist"
-    packages = [os.path.dirname(p) for p in glob.glob("azure*/setup.py")]
-    for package_name in packages:
-        command_sdist = "setup.py sdist --dist-dir={}".format(target_dir)
-        command_bdist_wheel = "setup.py bdist_wheel --dist-dir={}".format(target_dir)
-        check_call([sys.executable] + command_sdist.split(), cwd=package_name)
-        check_call([sys.executable] + command_bdist_wheel.split(), cwd=package_name)
+    target_dir = "./dist"
+    command_sdist = "setup.py sdist --dist-dir={}".format(target_dir)
+    command_bdist_wheel = "setup.py bdist_wheel --dist-dir={}".format(target_dir)
+    check_call([sys.executable] + command_sdist.split())
+    check_call([sys.executable] + command_bdist_wheel.split())
