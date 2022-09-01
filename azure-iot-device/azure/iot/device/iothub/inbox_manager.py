9e53cb3ee1af3d745c7cec6d049e03499e8339f9
Carter Tinney <catinney@microsoft.com>
2022-09-01 11:51:23 -0700

    removed unique method request inbox functionality

--- azure-iot-device/azure/iot/device/iothub/inbox_manager.py
+++ azure-iot-device/azure/iot/device/iothub/inbox_manager.py
@@ -13,10 +13,10 @@ logger = logging.getLogger(__name__)
 class InboxManager(object):
     """Manages the various Inboxes for a client.
 
-    :ivar c2d_message_inbox: The C2D message Inbox.
-    :ivar input_message_inboxes: A dictionary mapping input names to input message Inboxes.
-    :ivar generic_method_request_inbox: The generic method request Inbox.
-    :ivar named_method_request_inboxes: A dictionary mapping method names to method request Inboxes.
+    :ivar message_inbox: The inbox for messages.
+    :ivar method_request_inbox: The inbox for method requests.
+    :ivar twin_patch_inbox: The inbox for twin patches.
+    :ivar client_event_inbox: The inbox for client events.
     """
 
     def __init__(self, inbox_type):
@@ -26,20 +26,15 @@ class InboxManager(object):
         """
         self._create_inbox = inbox_type
         self.message_inbox = self._create_inbox()
-        self.generic_method_request_inbox = self._create_inbox()
+        self.method_request_inbox = self._create_inbox()
         self.twin_patch_inbox = self._create_inbox()
         self.client_event_inbox = self._create_inbox()
-
-        # These inboxes are used only for non-unified receives, using APIs which are now
-        # deprecated on the client. However we need to keep them functional for backwards
-        # compatibility
-        self.named_method_request_inboxes = {}
 
     def get_message_inbox(self):
         """Retrieve the Inbox for all messages (C2D and Input)"""
         return self.message_inbox
 
-    def get_method_request_inbox(self, method_name=None):
+    def get_method_request_inbox(self):
         """Retrieve the method request Inbox for a given method name if provided,
         or for generic method requests if not.
 
@@ -49,17 +44,7 @@ class InboxManager(object):
         associated Inbox is desired.
         :returns: An Inbox for method requests.
         """
-        if method_name:
-            try:
-                inbox = self.named_method_request_inboxes[method_name]
-            except KeyError:
-                # Create a new Inbox for the method name
-                inbox = self._create_inbox()
-                self.named_method_request_inboxes[method_name] = inbox
-        else:
-            inbox = self.generic_method_request_inbox
-
-        return inbox
+        return self.method_request_inbox
 
     def get_twin_patch_inbox(self):
         """Retrieve the Inbox for twin patches that arrive from the service
@@ -77,9 +62,7 @@ class InboxManager(object):
 
     def clear_all_method_requests(self):
         """Delete all method requests currently in inboxes."""
-        self.generic_method_request_inbox.clear()
-        for inbox in self.named_method_request_inboxes.values():
-            inbox.clear()
+        self.method_request_inbox.clear()
 
     def route_input_message(self, incoming_message):
         """Route an incoming input message
@@ -118,11 +101,7 @@ class InboxManager(object):
 
         :returns: Boolean indicating if the method request was successfully routed or not.
         """
-        try:
-            inbox = self.named_method_request_inboxes[incoming_method_request.name]
-        except KeyError:
-            inbox = self.generic_method_request_inbox
-        inbox.put(incoming_method_request)
+        self.method_request_inbox.put(incoming_method_request)
         return True
 
     def route_twin_patch(self, incoming_patch):
