339a0049548d5dce4f4a66bc76e3cac64d45dc78
Carter Tinney <catinney@microsoft.com>
2022-09-08 13:44:21 -0700

    implementation fix

--- azure-iot-device/azure/iot/device/iothub/sync_clients.py
+++ azure-iot-device/azure/iot/device/iothub/sync_clients.py
@@ -104,7 +104,7 @@ class GenericIoTHubClient(AbstractIoTHubClient):
         if not self._mqtt_pipeline.feature_enabled[feature_name]:
             callback = EventedCallback()
             self._mqtt_pipeline.enable_feature(feature_name, callback=callback)
-            callback.wait_for_completion()
+            handle_result(callback)
 
             logger.info("Successfully enabled feature:" + feature_name)
         else:
@@ -125,7 +125,7 @@ class GenericIoTHubClient(AbstractIoTHubClient):
             # Disable the feature if not already disabled
             callback = EventedCallback()
             self._mqtt_pipeline.disable_feature(feature_name, callback=callback)
-            callback.wait_for_completion()
+            handle_result(callback)
 
             logger.info("Successfully disabled feature: {}".format(feature_name))
         else:
