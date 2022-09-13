8ac131c71ffbac99275f5627c65c31ee92e7f4a7
olivakar <oliva.kar@microsoft.com>
2022-09-13 15:19:37 -0700

    fns

--- tests/e2e/provisioning_e2e/tests/test_async_certificate_enrollments.py
+++ tests/e2e/provisioning_e2e/tests/test_async_certificate_enrollments.py
@@ -19,12 +19,6 @@ from ..provisioningservice.protocol.models import (
 )
 from ..provisioningservice.client import ProvisioningServiceClient
 
-# from provisioningserviceclient import (
-#     ProvisioningServiceClient,
-#     IndividualEnrollment,
-#     EnrollmentGroup,
-# )
-# from provisioningserviceclient.protocol.models import AttestationMechanism, ReprovisionPolicy, X509CertificateWithInfo
 import pytest
 import logging
 import os
@@ -43,10 +37,6 @@ from ..provisioningservice.protocol.models import X509Certificates
 # pytestmark = [pytest.mark.skip, pytest.mark.asyncio]
 pytestmark = pytest.mark.asyncio
 logging.basicConfig(level=logging.DEBUG)
-logging.getLogger("azure-iot-device").setLevel(logging.DEBUG)
-# logging.getLogger('azure-iothub-provisioningserviceclient').setLevel(logging.DEBUG)
-logging.getLogger("azure-iot-hub").setLevel(logging.DEBUG)
-logging.getLogger("msrest").setLevel(logging.DEBUG)
 
 intermediate_common_name = "e2edpshomenum"
 intermediate_password = "revelio"
@@ -98,15 +88,18 @@ def before_all_tests(request):
 )
 @pytest.mark.parametrize("protocol", ["mqttws"])
 async def test_device_register_with_device_id_for_a_x509_individual_enrollment(protocol):
-    registration_id = ""
     device_id = "e2edpsthunderbolt"
     if protocol == "mqtt":
         device_index = type_to_device_indices.get("individual_with_device_id")[0]
     else:
         device_index = type_to_device_indices.get("individual_with_device_id_ws")[0]
+
+    registration_id = device_common_name + str(device_index)
     try:
+        cert_content = read_cert_content_from_file(device_index=device_index)
+
         individual_enrollment_record = create_individual_enrollment_with_x509_client_certs(
-            device_index=device_index, device_id=device_id
+            registration_id=registration_id, primary_cert=cert_content, device_id=device_id
         )
         registration_id = individual_enrollment_record.registration_id
 
@@ -128,16 +121,20 @@ async def test_device_register_with_device_id_for_a_x509_individual_enrollment(p
 )
 @pytest.mark.parametrize("protocol", ["mqtt", "mqttws"])
 async def test_device_register_with_no_device_id_for_a_x509_individual_enrollment(protocol):
-    registration_id = ""
+
     if protocol == "mqtt":
         device_index = type_to_device_indices.get("individual_no_device_id")[0]
     else:
         device_index = type_to_device_indices.get("individual_no_device_id_ws")[0]
 
+    registration_id = device_common_name + str(device_index)
     try:
+        cert_content = read_cert_content_from_file(device_index=device_index)
+
         individual_enrollment_record = create_individual_enrollment_with_x509_client_certs(
-            device_index=device_index
+            registration_id=registration_id, primary_cert=cert_content
         )
+
         registration_id = individual_enrollment_record.registration_id
 
         device_cert_file = "demoCA/newcerts/device_cert" + str(device_index) + ".pem"
@@ -172,16 +169,9 @@ async def test_group_of_devices_register_with_no_device_id_for_a_x509_intermedia
         with open(intermediate_cert_filename, "r") as intermediate_pem:
             intermediate_cert_content = intermediate_pem.read()
 
-        # attestation_mechanism = AttestationMechanism.create_with_x509_signing_certs(
-        #     intermediate_cert_content
-        # )
-        primary = X509CertificateWithInfo(certificate=intermediate_cert_content)
-        secondary = None
-        # if cert2:
-        #     X509CertificateWithInfo(certificate=cert2)
-        certs = X509Certificates(primary=primary, secondary=secondary)
-        x509 = X509Attestation(signing_certificates=certs)
-        # return cls(type="x509", x509=x509)
+        x509 = create_x509_client_or_sign_certs(
+            is_client=False, primary_cert=intermediate_cert_content
+        )
         attestation_mechanism = AttestationMechanism(type="x509", x509=x509)
         enrollment_group_provisioning_model = EnrollmentGroup(
             enrollment_group_id=group_id,
@@ -217,7 +207,6 @@ async def test_group_of_devices_register_with_no_device_id_for_a_x509_intermedia
             assert_device_provisioned(device_id=device_id, registration_result=registration_result)
             device_registry_helper.try_delete_device(device_id)
 
-        # Make sure space is okay. The following line must be outside for loop.
         assert count == device_count_in_group
 
     finally:
@@ -242,16 +231,7 @@ async def test_group_of_devices_register_with_no_device_id_for_a_x509_ca_authent
 
     try:
         DPS_GROUP_CA_CERT = os.getenv("PROVISIONING_ROOT_CERT")
-        # attestation_mechanism = AttestationMechanism.create_with_x509_ca_refs(
-        #     ref1=DPS_GROUP_CA_CERT
-        # )
-        primary = DPS_GROUP_CA_CERT
-        secondary = None
-        # if ref2:
-        #     secondary = ref2
-        ca_refs = X509CAReferences(primary=primary, secondary=secondary)
-        x509 = X509Attestation(ca_references=ca_refs)
-        # return cls(type="x509", x509=x509)
+        x509 = create_x509_ca_refs(primary_ref=DPS_GROUP_CA_CERT)
         attestation_mechanism = AttestationMechanism(type="x509", x509=x509)
         enrollment_group_provisioning_model = EnrollmentGroup(
             enrollment_group_id=group_id,
@@ -291,7 +271,6 @@ async def test_group_of_devices_register_with_no_device_id_for_a_x509_ca_authent
             assert_device_provisioned(device_id=device_id, registration_result=registration_result)
             device_registry_helper.try_delete_device(device_id)
 
-        # Make sure space is okay. The following line must be outside for loop.
         assert count == device_count_in_group
     finally:
         service_client.delete_enrollment_group_by_param(group_id)
@@ -313,22 +292,13 @@ def assert_device_provisioned(device_id, registration_result):
     assert device.device_id == device_id
 
 
-def create_individual_enrollment_with_x509_client_certs(device_index, device_id=None):
-    registration_id = device_common_name + str(device_index)
+def create_individual_enrollment_with_x509_client_certs(
+    registration_id, primary_cert, secondary_cert=None, device_id=None
+):
     reprovision_policy = ReprovisionPolicy(migrate_device_data=True)
-
-    device_cert_input_file = "demoCA/newcerts/device_cert" + str(device_index) + ".pem"
-    with open(device_cert_input_file, "r") as in_device_cert:
-        device_cert_content = in_device_cert.read()
-
-    # attestation_mechanism = AttestationMechanism.create_with_x509_client_certs(device_cert_content)
-    primary = X509CertificateWithInfo(certificate=device_cert_content)
-    secondary = None
-    # if cert2:
-    #     secondary = X509CertificateWithInfo(certificate=cert2)
-    certs = X509Certificates(primary=primary, secondary=secondary)
-    x509 = X509Attestation(client_certificates=certs)
-    # return cls(type="x509", x509=x509)
+    x509 = create_x509_client_or_sign_certs(
+        is_client=True, primary_cert=primary_cert, secondary_cert=secondary_cert
+    )
 
     attestation_mechanism = AttestationMechanism(type="x509", x509=x509)
 
@@ -340,6 +310,33 @@ def create_individual_enrollment_with_x509_client_certs(device_index, device_id=
     )
 
     return service_client.create_or_update_individual_enrollment(individual_provisioning_model)
+
+
+def create_x509_client_or_sign_certs(is_client, primary_cert, secondary_cert=None):
+
+    primary = X509CertificateWithInfo(certificate=primary_cert)
+    secondary = None
+    if secondary_cert:
+        secondary = X509CertificateWithInfo(certificate=secondary_cert)
+    certs = X509Certificates(primary=primary, secondary=secondary)
+    if is_client:
+        x509_attestation = X509Attestation(client_certificates=certs)
+    else:
+        x509_attestation = X509Attestation(signing_certificates=certs)
+    return x509_attestation
+
+
+def create_x509_ca_refs(primary_ref, secondary_ref=None):
+    ca_refs = X509CAReferences(primary=primary_ref, secondary=secondary_ref)
+    x509_attestation = X509Attestation(ca_references=ca_refs)
+    return x509_attestation
+
+
+def read_cert_content_from_file(device_index):
+    device_cert_input_file = "demoCA/newcerts/device_cert" + str(device_index) + ".pem"
+    with open(device_cert_input_file, "r") as in_device_cert:
+        device_cert_content = in_device_cert.read()
+    return device_cert_content
 
 
 async def result_from_register(registration_id, device_cert_file, device_key_file, protocol):
