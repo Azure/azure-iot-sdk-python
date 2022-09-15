f28005ac218587d2a4bb78d89878cd9f4e0dfb30
Carter Tinney <catinney@microsoft.com>
2022-09-15 10:16:33 -0700

    Added unittests for feature enable failure

--- tests/unit/iothub/shared_client_tests.py
+++ tests/unit/iothub/shared_client_tests.py
@@ -16,6 +16,7 @@ from azure.iot.device.common import auth, handle_exceptions
 from azure.iot.device.common.auth import sastoken as st
 from azure.iot.device.common.auth import connection_string as cs
 from azure.iot.device.iothub.pipeline import IoTHubPipelineConfig
+from azure.iot.device.iothub.pipeline import exceptions as pipeline_exceptions
 from azure.iot.device.common.pipeline.config import DEFAULT_KEEPALIVE
 from azure.iot.device.iothub.abstract_clients import (
     RECEIVE_TYPE_NONE_SET,
@@ -702,6 +703,67 @@ class SharedIoTHubClientPROPERTYReceiverHandlerTests(SharedIoTHubClientPROPERTYH
         # Feature was not enabled again
         assert mqtt_pipeline.enable_feature.call_count == 0
 
+    @pytest.mark.it("Raises a client error if enabling the corresponding feature fails")
+    @pytest.mark.parametrize(
+        "pipeline_error,client_error",
+        [
+            pytest.param(
+                pipeline_exceptions.ConnectionDroppedError,
+                client_exceptions.ConnectionDroppedError,
+                id="ConnectionDroppedError->ConnectionDroppedError",
+            ),
+            pytest.param(
+                pipeline_exceptions.ConnectionFailedError,
+                client_exceptions.ConnectionFailedError,
+                id="ConnectionFailedError->ConnectionFailedError",
+            ),
+            pytest.param(
+                pipeline_exceptions.NoConnectionError,
+                client_exceptions.NoConnectionError,
+                id="NoConnectionError->NoConnectionError",
+            ),
+            pytest.param(
+                pipeline_exceptions.UnauthorizedError,
+                client_exceptions.CredentialError,
+                id="UnauthorizedError->CredentialError",
+            ),
+            pytest.param(
+                pipeline_exceptions.ProtocolClientError,
+                client_exceptions.ClientError,
+                id="ProtocolClientError->ClientError",
+            ),
+            pytest.param(
+                pipeline_exceptions.OperationCancelled,
+                client_exceptions.OperationCancelled,
+                id="OperationCancelled -> OperationCancelled",
+            ),
+            pytest.param(
+                pipeline_exceptions.OperationTimeout,
+                client_exceptions.OperationTimeout,
+                id="OperationTimeout -> OperationTimeout",
+            ),
+            pytest.param(Exception, client_exceptions.ClientError, id="Exception->ClientError"),
+        ],
+    )
+    def test_enable_feature_fails(
+        self, mocker, client, handler, handler_name, mqtt_pipeline, pipeline_error, client_error
+    ):
+        # Feature will appear disabled
+        mqtt_pipeline.feature_enabled.__getitem__.return_value = False
+
+        # Enable Feature will fail
+        my_pipeline_error = pipeline_error()
+
+        def fail_enable_feature(feature_name, callback):
+            callback(error=my_pipeline_error)
+
+        mqtt_pipeline.enable_feature = mocker.MagicMock(side_effect=fail_enable_feature)
+
+        # Set handler
+        with pytest.raises(client_error) as e_info:
+            setattr(client, handler_name, handler)
+        assert e_info.value.__cause__ is my_pipeline_error
+
     @pytest.mark.it(
         "Implicitly disables the corresponding feature if not already disabled, when handler value is set back to None"
     )
@@ -724,6 +786,67 @@ class SharedIoTHubClientPROPERTYReceiverHandlerTests(SharedIoTHubClientPROPERTYH
         setattr(client, handler_name, None)
         # Feature was not disabled again
         assert mqtt_pipeline.disable_feature.call_count == 0
+
+    @pytest.mark.it("Raises a client error if disabling the corresponding feature fails")
+    @pytest.mark.parametrize(
+        "pipeline_error,client_error",
+        [
+            pytest.param(
+                pipeline_exceptions.ConnectionDroppedError,
+                client_exceptions.ConnectionDroppedError,
+                id="ConnectionDroppedError->ConnectionDroppedError",
+            ),
+            pytest.param(
+                pipeline_exceptions.ConnectionFailedError,
+                client_exceptions.ConnectionFailedError,
+                id="ConnectionFailedError->ConnectionFailedError",
+            ),
+            pytest.param(
+                pipeline_exceptions.NoConnectionError,
+                client_exceptions.NoConnectionError,
+                id="NoConnectionError->NoConnectionError",
+            ),
+            pytest.param(
+                pipeline_exceptions.UnauthorizedError,
+                client_exceptions.CredentialError,
+                id="UnauthorizedError->CredentialError",
+            ),
+            pytest.param(
+                pipeline_exceptions.ProtocolClientError,
+                client_exceptions.ClientError,
+                id="ProtocolClientError->ClientError",
+            ),
+            pytest.param(
+                pipeline_exceptions.OperationCancelled,
+                client_exceptions.OperationCancelled,
+                id="OperationCancelled -> OperationCancelled",
+            ),
+            pytest.param(
+                pipeline_exceptions.OperationTimeout,
+                client_exceptions.OperationTimeout,
+                id="OperationTimeout -> OperationTimeout",
+            ),
+            pytest.param(Exception, client_exceptions.ClientError, id="Exception->ClientError"),
+        ],
+    )
+    def test_disable_feature_fails(
+        self, mocker, client, handler, handler_name, mqtt_pipeline, pipeline_error, client_error
+    ):
+        # Feature will appear enabled
+        mqtt_pipeline.feature_enabled.__getitem__.return_value = True
+
+        # Disable Feature will fail
+        my_pipeline_error = pipeline_error()
+
+        def fail_disable_feature(feature_name, callback):
+            callback(error=my_pipeline_error)
+
+        mqtt_pipeline.disable_feature = mocker.MagicMock(side_effect=fail_disable_feature)
+
+        # Set handler to None
+        with pytest.raises(client_error) as e_info:
+            setattr(client, handler_name, None)
+        assert e_info.value.__cause__ is my_pipeline_error
 
     @pytest.mark.it(
         "Locks the client to Handler Receive Mode if the receive mode has not yet been set"
