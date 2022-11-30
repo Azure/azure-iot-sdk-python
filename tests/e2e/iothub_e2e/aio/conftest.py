dbb0e02c015cf46e6b57ca8246651a9b7d99f5ac
Carter Tinney <catinney@microsoft.com>
2022-11-30 15:25:46 -0800

    Updated event loop fixture

--- tests/e2e/iothub_e2e/aio/conftest.py
+++ tests/e2e/iothub_e2e/aio/conftest.py
@@ -57,8 +57,9 @@ def pytest_sessionfinish(session, exitstatus):
 
 
 @pytest.fixture(scope="session")
-async def event_loop():
-    loop = asyncio.get_running_loop()
+def event_loop():
+    policy = asyncio.get_event_loop_policy()
+    loop = policy.new_event_loop()
     yield loop
     loop.close()
 
