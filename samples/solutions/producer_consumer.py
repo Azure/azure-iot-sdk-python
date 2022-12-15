45535e38c4093668462c6c4d24b8c449f335e400
Oliva Kar <olkar@microsoft.com>
2022-12-15 13:15:10 -0800

    added logging

--- samples/solutions/producer_consumer.py
+++ samples/solutions/producer_consumer.py
@@ -1,6 +1,6 @@
 # CUSTOMER PERSONA
 # A producer application is creating a message and inserting inside a queue uniformly.
-# Customer wants to fetch a message from a queue and send message at some interval consistently
+# Customer wants to fetch a message from a queue and send the message at some interval consistently
 # as long as connection remains. In case of disconnection the customer wants to retry the connection
 # for errors that are worth retrying.
 
@@ -8,23 +8,62 @@ import asyncio
 import logging
 from azure.iot.device.iothub.aio import IoTHubDeviceClient
 from azure.iot.device import exceptions, Message
-import traceback
 import os
-
-
-logging.basicConfig(
-    level=logging.DEBUG,
-    format="%(asctime)s %(levelname)-5s (%(threadName)s) %(filename)s:%(funcName)s():%(message)s",
-    filename="device_60_hours.log",
-)
-
+import logging.handlers
+import glob
 
 # The interval at which to send telemetry
 TELEMETRY_INTERVAL = 10
-# Number of times connection attempts will be made.
-RETRY_NOS = 50
-# Interval between consecutive connection attempts
+# Interval in sec between consecutive connection attempts
 ATTEMPT_INTERVAL = 5
+# Interval for rotating logs, in seconds
+LOG_ROTATION_INTERVAL = 3600
+# How many logs to keep before recycling
+LOG_BACKUP_COUNT = 6
+# Directory for storing log files
+LOG_DIRECTORY = "./logs"
+
+# Prepare the log directory
+os.makedirs(LOG_DIRECTORY, exist_ok=True)
+for filename in glob.glob("{}/*.log".format(LOG_DIRECTORY)):
+    os.remove(filename)
+
+log_formatter = logging.Formatter(
+    "%(asctime)s %(levelname)-5s (%(threadName)s) %(filename)s:%(funcName)s():%(message)s"
+)
+
+info_log_handler = logging.handlers.TimedRotatingFileHandler(
+    filename="{}/info.log".format(LOG_DIRECTORY),
+    when="S",
+    interval=LOG_ROTATION_INTERVAL,
+    backupCount=LOG_BACKUP_COUNT,
+)
+info_log_handler.setLevel(level=logging.INFO)
+info_log_handler.setFormatter(log_formatter)
+
+debug_log_handler = logging.handlers.TimedRotatingFileHandler(
+    filename="{}/debug.log".format(LOG_DIRECTORY),
+    when="S",
+    interval=LOG_ROTATION_INTERVAL,
+    backupCount=LOG_BACKUP_COUNT,
+)
+debug_log_handler.setLevel(level=logging.DEBUG)
+debug_log_handler.setFormatter(log_formatter)
+
+root_logger = logging.getLogger()
+root_logger.setLevel(level=logging.DEBUG)
+root_logger.addHandler(info_log_handler)
+root_logger.addHandler(debug_log_handler)
+
+sample_log_handler = logging.FileHandler(filename="{}/sample.log".format(LOG_DIRECTORY))
+sample_log_handler.setLevel(level=logging.DEBUG)
+sample_log_handler.setFormatter(log_formatter)
+logger = logging.getLogger(__name__)
+logger.addHandler(sample_log_handler)
+
+
+def get_type_name(e):
+    return type(e).__name__
 
 
 class Application(object):
@@ -42,7 +81,6 @@ class Application(object):
 
         self.connected_event = asyncio.Event()
         self.disconnected_event = asyncio.Event()
-        # self.exit_app_event = asyncio.Event()
         self.disconnected_event.set()
 
         self.message_queue = asyncio.Queue()
@@ -55,23 +93,23 @@ class Application(object):
             self.device_client = IoTHubDeviceClient.create_from_connection_string(conn_str)
             # Attach the connection state handler
             self.device_client.on_connection_state_change = self.handle_on_connection_state_change
-        except Exception:
-            # Clean up in the connected_event of failure
-            print("Caught exception while trying to attach handler....")
-            logging.debug(traceback.format_exc())
+        except Exception as e:
+            self.log_error_and_print(
+                "Caught exception while trying to attach handler : {}".format(get_type_name(e))
+            )
 
     async def handle_on_connection_state_change(self):
-        print(
+        self.log_info_and_print(
             "handle_on_connection_state_change fired. Connected status : {}".format(
                 self.device_client.connected
             )
         )
         if self.device_client.connected:
-            print("Connected connected_event is set...")
+            self.log_info_and_print("Connected connected_event is set...")
             self.disconnected_event.clear()
             self.connected_event.set()
         else:
-            print("Disconnected connected_event is set...")
+            self.log_info_and_print("Disconnected connected_event is set...")
             self.disconnected_event.set()
             self.connected_event.clear()
 
@@ -81,7 +119,7 @@ class Application(object):
             msg = Message("current wind speed ")
             msg.message_id = message_id
             msg.content_type = "application/json"
-            print("Created a message...")
+            self.log_info_and_print("Created a message...")
             self.message_queue.put_nowait(msg)
             await asyncio.sleep(TELEMETRY_INTERVAL)
             message_id += 1
@@ -89,25 +127,25 @@ class Application(object):
     async def wait_for_connect_and_send_telemetry(self):
         while True:
             if not self.device_client.connected:
-                print("waiting for connection ...")
+                self.log_info_and_print("waiting for connection ...")
                 await self.connected_event.wait()
             else:
                 msg = await self.message_queue.get()
                 try:
-                    print("sending message...")
+                    self.log_info_and_print("sending message...")
                     await self.device_client.send_message(msg)
-                    print("sent message")
+                    self.log_info_and_print("sent message")
                     self.message_queue.task_done()
                     await asyncio.sleep(TELEMETRY_INTERVAL)
-                except Exception:
-                    print("Caught exception while trying to send message....")
-                    logging.debug(traceback.format_exc())
+                except Exception as e:
+                    self.log_error_and_print(
+                        "Caught exception while trying to send message: {}".format(get_type_name(e))
+                    )
                     self.message_queue.put_nowait(msg)
 
-    async def if_disconnected_then_connect_with_retry(self, number_of_tries=None):
+    async def if_disconnected_then_connect_with_retry(self):
         i = 0
         while True:
-            # for i in range(number_of_tries):
             sleep_time = 0
             await self.disconnected_event.wait()
             if self.first_connect:
@@ -116,27 +154,31 @@ class Application(object):
                 sleep_time = ATTEMPT_INTERVAL
             if not self.device_client.connected:
                 try:
-                    print("Attempting to connect the device client try number {}....".format(i))
+                    self.log_info_and_print(
+                        "Attempting to connect the device client try number {}....".format(i)
+                    )
                     await self.device_client.connect()
-                    print("Successfully connected the device client...")
+                    self.log_info_and_print("Successfully connected the device client...")
                     i = 0
                 except Exception as e:
-                    print("Caught exception while trying to connect...")
+                    self.log_error_and_print("Caught exception while trying to connect...")
                     if type(e) is exceptions.CredentialError:
-                        print(
+                        self.log_error_and_print(
                             "Failed to connect the device client due to incorrect or badly formatted credentials..."
                         )
                         i = 0
                         raise
 
                     if self.is_retryable(e):
-                        print(
+                        self.log_error_and_print(
                             "Failed to connect the device client due to retryable error.Sleeping and retrying after some time..."
                         )
                         i += 1
                         await asyncio.sleep(sleep_time)
                     else:
-                        print("Failed to connect the device client due to not-retryable error....")
+                        self.log_error_and_print(
+                            "Failed to connect the device client due to not-retryable error...."
+                        )
                         i = 0
                         raise
 
@@ -146,6 +188,14 @@ class Application(object):
         else:
             return False
 
+    def log_error_and_print(self, s):
+        logger.error(s)
+        print(s)
+
+    def log_info_and_print(self, s):
+        logger.info(s)
+        print(s)
+
     async def main(self):
         conn_str = os.getenv("IOTHUB_DEVICE_CONNECTION_STRING")
         await self.initiate()
@@ -154,19 +204,18 @@ class Application(object):
         tasks = [
             asyncio.create_task(self.wait_for_connect_and_send_telemetry()),
             asyncio.create_task(self.enqueue_message()),
-            asyncio.create_task(self.if_disconnected_then_connect_with_retry(RETRY_NOS)),
+            asyncio.create_task(self.if_disconnected_then_connect_with_retry()),
         ]
 
         try:
             done, pending = await asyncio.wait(tasks, return_when=asyncio.FIRST_EXCEPTION)
             await asyncio.gather(*done)
-        except (KeyboardInterrupt, Exception):
-            print("IoTHubClient sample stopped by user")
-        except Exception:
-            print("Exception in main loop...")
-            logging.debug(traceback.format_exc())
+        except KeyboardInterrupt:
+            self.log_error_and_print("IoTHubClient sample stopped by user")
+        except Exception as e:
+            self.log_error_and_print("Exception in main loop: {}".format(get_type_name(e)))
         finally:
-            print("Shutting down IoTHubClient and exiting Application")
+            self.log_info_and_print("Shutting down IoTHubClient and exiting Application")
             await self.device_client.shutdown()
 
 
