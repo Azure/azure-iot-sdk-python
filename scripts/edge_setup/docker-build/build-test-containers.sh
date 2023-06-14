563abbc276166e02bf0d9a62f626fd6195f1c44c
Bert Kleewein <bertk@micosoft.com>
2023-06-14 11:17:49 -0700

    move root dir in script

--- scripts/edge_setup/docker-build/build-test-containers.sh
+++ scripts/edge_setup/docker-build/build-test-containers.sh
@@ -1,7 +1,7 @@
 # Copyright (c) Microsoft. All rights reserved.
 # Licensed under the MIT license. See LICENSE file in the project root for full license information.
 script_dir=$(cd "$(dirname "$0")" && pwd)
-root_dir=$(cd "${script_dir}/../.." && pwd)
+root_dir=$(cd "${script_dir}/../../.." && pwd)
 
 DOCKERFILE_NAME=$1
 IMAGE_NAME=$2
