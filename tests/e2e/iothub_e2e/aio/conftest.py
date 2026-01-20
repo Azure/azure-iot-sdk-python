35e2387b7030badca0b7de383edddd8ee560e916
Carter Tinney <carter.tinney@microsoft.com>
2026-01-20 11:00:12 -0800

    more legacy config

--- tests/e2e/iothub_e2e/aio/conftest.py
+++ tests/e2e/iothub_e2e/aio/conftest.py
@@ -55,13 +55,6 @@ def pytest_sessionfinish(session, exitstatus):
             print("-----------------------------------")
 
 
-# @pytest.fixture(scope="session")
-# def event_loop():
-#     loop = asyncio.get_event_loop()
-#     yield loop
-#     loop.close()
-
-
 @pytest.fixture(scope="function")
 async def brand_new_client(device_identity, client_kwargs, service_helper, device_id, module_id):
     service_helper.set_identity(device_id, module_id)
@@ -108,7 +101,6 @@ async def service_helper(executor):
         iothub_connection_string=test_env.IOTHUB_CONNECTION_STRING,
         eventhub_connection_string=test_env.EVENTHUB_CONNECTION_STRING,
         eventhub_consumer_group=test_env.EVENTHUB_CONSUMER_GROUP,
-        # event_loop=event_loop,
         executor=executor,
     )
     yield service_helper
