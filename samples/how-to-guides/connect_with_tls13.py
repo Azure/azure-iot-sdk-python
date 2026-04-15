48a40df3edfe825324addb5a59586068132a3948
Tim Taylor <timtay@microsoft.com>
2026-04-15 11:14:20 -0700

    Add a sample that demonstrates TLS 1.3 support (#1224)
    * Add sample for TLS 1.3 support
    
    * more
    
    * note
    
    * simplify sample
    
    * comments

---
+++ samples/how-to-guides/connect_with_tls13.py
@@ -0,0 +1,32 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
+
+import os
+from azure.iot.device import IoTHubDeviceClient, Message
+
+# The device connection string to authenticate the device with your IoT hub.
+CONNECTION_STRING = os.getenv("IOTHUB_DEVICE_CONNECTION_STRING")
+
+if ".device.azure-devices." not in CONNECTION_STRING:
+    # classic connection strings that look like '<hub-name>.azure-devices.<dns suffix>' only support up to TLS 1.2
+    raise ValueError(
+        "Device connection string must match the format '<hub name>.device.azure-devices.<dns suffix>' in order to use TLS 1.3"
+    )
+
+# The client object is used to interact with your Azure IoT hub.
+device_client = IoTHubDeviceClient.create_from_connection_string(CONNECTION_STRING)
+
+# Connect the client.
+print("opening the connection")
+device_client.connect()
+
+print("sending message")
+msg = Message("Hello from TLS 1.3 connection!")
+device_client.send_message(msg)
+
+# finally, shut down the client
+print("closing the connection")
+device_client.shutdown()
