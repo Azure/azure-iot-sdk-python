03eb2952853c355e7f1ed2d2ebcc5739244ec18d
Carter Tinney <catinney@microsoft.com>
2026-05-11 14:10:20 -0700

    enabled all twin;r

--- tests/e2e/iothub_e2e/sync/test_sync_twin.py
+++ tests/e2e/iothub_e2e/sync/test_sync_twin.py
@@ -22,7 +22,6 @@ reset_reported_props = {const.TEST_CONTENT: None}
 class TestReportedProperties(object):
     @pytest.mark.it("Can set a simple reported property")
     @pytest.mark.quicktest_suite
-    # @pytest.mark.skip(reason="Disabling as tests are failing. Needs investigation.")
     def test_sync_sends_simple_reported_patch(
         self, client, random_reported_props, service_helper, leak_tracker
     ):
@@ -56,7 +55,6 @@ class TestReportedProperties(object):
 
     @pytest.mark.it("Can clear a reported property")
     @pytest.mark.quicktest_suite
-    # @pytest.mark.skip(reason="Disabling as tests are failing. Needs investigation.")
     def test_sync_clear_property(self, client, random_reported_props, service_helper, leak_tracker):
         leak_tracker.set_initial_object_list()
 
@@ -82,7 +80,6 @@ class TestReportedProperties(object):
 
     @pytest.mark.it("Connects the transport if necessary")
     @pytest.mark.quicktest_suite
-    # @pytest.mark.skip(reason="Disabling as tests are failing. Needs investigation.")
     def test_sync_patch_reported_connect_if_necessary(
         self, client, random_reported_props, service_helper, leak_tracker
     ):
@@ -109,7 +106,6 @@ class TestReportedProperties(object):
 @pytest.mark.dropped_connection
 @pytest.mark.describe("Client Reported Properties with dropped connection")
 @pytest.mark.keep_alive(5)
-@pytest.mark.skip(reason="Disabling as tests are failing. Needs investigation.")
 class TestReportedPropertiesDroppedConnection(object):
 
     # TODO: split drop tests between first and second patches
@@ -176,7 +172,6 @@ class TestReportedPropertiesDroppedConnection(object):
 
 
 @pytest.mark.describe("Client Desired Properties")
-@pytest.mark.skip(reason="Disabling as tests are failing. Needs investigation.")
 class TestDesiredProperties(object):
     @pytest.mark.it("Receives a patch for a simple desired property")
     @pytest.mark.quicktest_suite
